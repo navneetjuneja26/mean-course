acd3b66852dc3f27e8ed230f006f3c6ed0af08a0
Van Dillon <vdillon@edge2web.com>
2018-09-25 14:26:02 -0400

    Calling GET Post

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,19 +1,26 @@
 import { Post } from './post.model';
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 // Does the same thing as addiing sevice to app.module providers
 // Injected srvice is a singleton
 @Injectable({providedIn: 'root'})
 export class PostsService {
   private posts: Post[] = [];
+  private postsUpdated = new Subject<Post[]>();
 
   getPosts() {
     // return copy of array instead of reference
     return [...this.posts];
   }
 
+  getPostsUpdateListener() {
+    return this.postsUpdated.asObservable();
+  }
+
   addPost(title: string, content: string) {
     const post: Post = {title: title, content: content};
     this.posts.push(post);
+    this.postsUpdated.next([...this.posts]);
   }
 }
