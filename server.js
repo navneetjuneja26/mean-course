7645cd102c40209c4b98e2626cd1a4111dd92bbc
Van Dillon <vdillon@edge2web.com>
2018-10-06 23:16:21 -0400

    Adding the Express Framework

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 console.log('Node server starting...');
 
 const http = require('http');
+const app = require('./backend/app')
 
-const server = http.createServer((req, res) => {
-  res.end('This is my first response');
-});
+const port = process.env.PORT || 3000;
 
-server.listen(process.env.PORT || 3000);
+app.set('port', )
+const server = http.createServer(app);
+
+server.listen(port);
