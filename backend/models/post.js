98dee269d1048e7a735ded7c5d38a855a931ecbe
Van Dillon <vdillon@edge2web.com>
2018-11-03 11:02:02 -0400

    Understanding Mongoose Schemas & Models

--- backend/models/post.js
+++ backend/models/post.js
@@ -7,4 +7,4 @@ const postSchema = mongoose.Schema({
   content: { type: String, required: true }
 });
 
-module.exports mongoose.model('Post', postSchema);
+module.exports = mongoose.model('Post', postSchema);
